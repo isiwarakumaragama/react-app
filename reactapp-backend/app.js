067160589811d3c4f8413fa86a7877d3c29e9024
Isiwara Kumaragama <manujaisiwarakumaragama@gmail.com>
2024-07-25 16:00:16 +0530

    create CRUD operations

--- reactapp-backend/app.js
+++ reactapp-backend/app.js
@@ -15,15 +15,26 @@ app.use(
 app.use(express.json());
 
 app.get('/users', (req, res) => {
-    controller.getUsers(users => {
-        res.send(users);
+    controller.getUsers( (req, res, next ) => {
+        res.send();
     });
 });
 
-app.get('/user', (req, res) => {
-    const id = req.query.id;
-    controller.getUserById(id, user => {
-        res.send(user);
+app.post('/createuser', (req, res) => {
+    controller.addUser(req.body, (callack) => {
+        res.send();
+    });
+});
+
+app.post('/updateuser', (req, res) => {
+    controller.updateUser(req.body, (callack) => {
+        res.send(callack);
+    });
+});
+
+app.post('/deleteuser', (req, res) => {
+    controller.deleteUser(req.body, (callack) => {
+        res.send(callack);
     });
 });
 
--- reactapp-backend/server.js
+++ reactapp-backend/server.js
@@ -1,8 +1,32 @@
-const app = require('./app');
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const port = 3001;
 const host = '127.0.0.1';
+const mongoose = require('mongoose');
+const router = require('./router');
+
+app.use(cors());
+app.use(express.json());
+
+const uri = 'mongodb+srv://misiwara:isiwara000@cluster0.lpnregv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const connect = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log('connected to mongo db')
+    }
+    catch(error) {
+        console.log('mongo db error: ',error);
+    }
+};
+
+connect();
+
 
 const server = app.listen(port, host, () => {
     console.log(`Node server is listening to ${server.address().port}`);
 });
+
+app.use('/api', router);
 
